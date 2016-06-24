fb450807433398804b2afe4c8b64db218141ab5f
MartinRoth <marsianus@gmail.com>
2016-06-24 15:23:55 +0200

    Introduced function ReadChangeFactors

--- R/temperatuur_transformatie_KNMI14.R
+++ R/temperatuur_transformatie_KNMI14.R
@@ -116,6 +116,7 @@
 #'                <MON> Middenoost Nederland
 #'                <ZON> Zuidoost Nederland
 #'
+#' @importFrom data.table fread
 #' @export
 temperatuur_transformatie_KNMI14 <- function(ifile,
                                               ofile="uitvoer.txt",
@@ -142,23 +143,7 @@ temperatuur_transformatie_KNMI14 <- function(ifile,
 #print(obs[1:10,])
 
   # READ CHANGE FACTORS (DELTAS)
-  if(!is.na(delta.file)) {
-    deltas <- read.table(delta.file,header=T)         # deltas are provided in file "delta.file"
-  } else {
-    if(p=="2030") {
-      tmpPath <- paste("deltas-KNMI14__",var,"_______2030.txt",sep="")
-      tmpPath <- system.file("extdata", tmpPath,
-                             package="knmitransformer")
-      deltas <- read.table(tmpPath, header=T) # 2030 decadal prediction if p=2030
-    } else {
-      # str.ext is a function used to construct file.names if delta.file is not explicitly provided
-      str.ext <- function(var,ch,n) {paste(var,substr(paste(rep(ch,n),collapse=""),1,n-nchar(var)),sep="")}
-
-      deltas <- read.table(paste("deltas-KNMI14__",var,"___",
-                                 str.ext( sc,"_",3),"_",
-                                 str.ext(  p,"_",4),".txt",sep=""),header=T)
-    }
-  }
+  deltas <- ReadChangeFactors(delta.file, var, sc, p)
 
 #print(deltas)
 
@@ -212,6 +197,6 @@ temperatuur_transformatie_KNMI14 <- function(ifile,
 #JB<
 
   sink()
+  fread(ofile)
 
-
-  } # end function temp.trans_KNMI14
+} # end function temp.trans_KNMI14
