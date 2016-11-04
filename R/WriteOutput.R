627794328fd6f6211c0911b07c32619317aac159
MartinRoth <marsianus@gmail.com>
2016-11-04 15:08:47 +0100

    Use WriteOutput function also for temperature data

--- R/WriteOutput.R
+++ R/WriteOutput.R
@@ -3,17 +3,21 @@ WriteOutput <- function(var, ofile, version, sc, p, H.comments, header, fut) {
   sink(ofile)
 
   # comments
-  writeLines("# Transformed daily mean global radiation [kJ/m2] according to KNMI'14 transformation tool,")
-  writeLines(paste("# version ",version,sep=""))
-  if(is.na(p)) {
-    writeLines(paste("# Deltas are derived from ",sc,sep=""))
+  if (var == "rsds") {
+    writeLines("# Transformed daily mean global radiation [kJ/m2] according to KNMI'14 transformation tool,")
+  } else if (var %in% c("tg", "tn", "tx")) {
+    writeLines("# Transformed daily temperature [deg.C] according to KNMI'14 transformation tool,")
   } else {
-    if(p=="2030") {
-      writeLines("# Deltas are derived from the 2030 decadal prediction")
-    } else {
-      writeLines(paste("# Deltas are derived from ",sc," scenario",sep=""))
-      writeLines(paste("# around the time horizon ",p,sep=""))
-    }
+    flog.error("variable not defined")
+    stop()
+  }
+
+  writeLines(paste("# version ",version,sep=""))
+  if(p=="2030") {
+    writeLines("# Deltas are derived from the 2030 decadal prediction")
+  } else {
+    writeLines(paste("# Deltas are derived from ", sc ," scenario", sep=""))
+    writeLines(paste("# around the time horizon ", p, sep=""))
   }
   writeLines("#")
   writeLines("# Bakker A. (2015), Time series transformation tool: description of the program to")
@@ -21,17 +25,9 @@ WriteOutput <- function(var, ofile, version, sc, p, H.comments, header, fut) {
   writeLines("#")
   for(i in 1:length(H.comments)) writeLines(H.comments[i])
 
-  # header
-  # write.table(format(header[1,],width=10,justify="right"),row.names=F,col.names=F,quote=F)
-  # write.table(format(header[-1,],width=10,justify="right"),row.names=F,col.names=F,quote=F)
-  # # transformed data
-  # write.table(format(fut,width=10,digits=2,justify="right"),row.names=F,col.names=F,quote=F)
-  #JB>
   write.table(format(header[1,], width = 8),              file = "", row.names = F, col.names = F, quote = F)
   write.table(format(header[-1,], width = 8, nsmall = 3), file = "", row.names = F, col.names = F, quote = F)
-  # transformed data
   write.table(format(fut, width = 8, nsmall = 2),         file = "", row.names = F, col.names = F, quote = F)
-  #JB<
 
   sink()
 
--- R/temperatuur_transformatie_KNMI14.R
+++ R/temperatuur_transformatie_KNMI14.R
@@ -171,37 +171,7 @@ temperatuur_transformatie_KNMI14 <- function(ifile,
   fut <- tm_trans_KNMI14(obs=obs, deltas=deltas, regio.tabel=regio.tabel)
 
   # OUTPUT #############################################################################################
-  sink(ofile)
-
-  # comments
-  writeLines("# Transformed daily temperature [deg.C] according to KNMI'14 transformation tool,")
-  writeLines(paste("# version ",version,sep=""))
-  if(p=="2030") {
-    writeLines("# Deltas are derived from the 2030 decadal prediction")
-  } else {
-    writeLines(paste("# Deltas are derived from ", sc ," scenario", sep=""))
-    writeLines(paste("# around the time horizon ", p, sep=""))
-  }
-  writeLines("#")
-  writeLines("# Bakker A. (2015), Time series transformation tool: description of the program to")
-  writeLines("# generate time series consistent with the KNMI'14 climate scenarios, TR-349")
-  writeLines("#")
-  for(i in 1:length(H.comments)) writeLines(H.comments[i])
-
-  # header
-# write.table(format(header[1,],width=10,justify="right"),row.names=F,col.names=F,quote=F)
-# write.table(format(header[-1,],width=10,justify="right"),row.names=F,col.names=F,quote=F)
-# # transformed data
-# write.table(format(fut,width=10,digits=2,justify="right"),row.names=F,col.names=F,quote=F)
-#JB>
-  write.table(format(header[1,], width = 8),              file = "", row.names = F, col.names = F, quote = F)
-  write.table(format(header[-1,], width = 8, nsmall = 3), file = "", row.names = F, col.names = F, quote = F)
-  # transformed data
-  write.table(format(fut, width = 8, nsmall = 2),         file = "", row.names = F, col.names = F, quote = F)
-#JB<
-
-  sink()
-  result <- fread(ofile)
+  result <- WriteOutput(var, ofile, version, sc, p, H.comments, header, fut)
 
   flog.debug("Temperature transformation ended successfully!")
   flog.debug("")
