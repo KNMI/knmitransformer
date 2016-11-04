8b8924a884656318b22c56273c11362901821812
MartinRoth <marsianus@gmail.com>
2016-11-04 14:25:19 +0100

    Reproduction of the web data for radiation (WH2085 used the old delta file)

--- tests/testthat/test-radiation.R
+++ tests/testthat/test-radiation.R
@@ -80,9 +80,10 @@ test_that("Scenario WH 2085", {
   sc="WH"
   p=2085 # This set is not produced by this version of the code - neither with
   # a factor of 0.7 or 0.75
+  oldDeltaFile = "regressionInput/radiation/deltas-KNMI14__rsds_WH__2085_v201504.txt"
   tmp <- straling_transformatie_KNMI14(ifile=ifile,
                                           ofile=ofile,
-                                          delta.file=delta.file,
+                                          delta.file=oldDeltaFile,
                                           sc=sc,
                                           p=p)
 
