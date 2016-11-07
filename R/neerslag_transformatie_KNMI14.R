59d92cb22ee80889d27037be57873bdbc7b7786e
MartinRoth <marsianus@gmail.com>
2016-11-07 11:43:08 +0100

    Updated description of transformation functions

--- R/neerslag_transformatie_KNMI14.R
+++ R/neerslag_transformatie_KNMI14.R
@@ -1,70 +1,16 @@
-###########################################################################################################
-#
-# neerslag_transformatie_KNMI14.R    March 10, 2015
-#
-# author: Alexander Bakker (KNMI)
-#
-# Function reads 'reference data' with daily precipitation sums [mm] and
-#                'change factors' from input files
-# and applies them to function 'rr_trans_KNMI14' to obtain
-# 'future time series' that match a certain climate
-#
-# Transformation developed for KNMI'14 climate change scenarios for the Netherlands:
-#
-# Bakker, A. (2015), Time series transformation tool: description of the program to generate time series
-# consistent with the KNMI’14 climate scenarios, Technical Report TR-349, De Bilt, the Netherlands
-#
-#
-# THIS CODE IS PROVIDED AS-IS WITH NO WARRANTY (NEITHER EXPLICIT
-# NOT IMPLICIT).  KNMI SHARES THIS CODE IN HOPES THAT IT IS USEFUL,
-# BUT KNMI IS NOT LIABLE FOR THE BEHAVIOR OF THIS CODE IN YOUR OWN
-# APPLICATION. YOU ARE FREE TO SHARE THIS CODE SO LONG AS THE
-# AUTHOR(S) AND VERSION HISTORY REMAIN INTACT.
-#
-#
-# arguments:
-#
-# ifile          Name of the input file (ASCII) that contains reference data (all numerics) in which
-#                the columns provide time series for specific stations. The first column
-#                should provide either 00000000 or a datestring YYYYMMDD:
-#                Rows starting with 00000000 are considered station info (station number, lat, lon
-#                etc.) and are ignored.
-#                Rows starting with a datestring refer to a specific day in the time series.
-#                Rows starting with "#" are completely ignored and returned unchanged
-#
-# ofile          (DEFAULT="uitvoer.txt") Name of the output file to write the transformed data to.
-#                Format is similar to ifile
-#
-# delta.file     [optional] Name of file that contains deltas (changes factors for the transformation)
-#                File should contain following compulsory columns identified with compulsory headers
-#                "wdf"       relative change in wet-day frequency
-#                            (wet days are defined as days with 0.1 mm or more precipitation)
-#                "ave"       relative change in mean precipitation
-#                "p99.lower" lower   estimate of the relative change in the 99th percentile of wet-day amounts
-#                "p99.centr" central estimate of the relative change in the 99th percentile of wet-day amounts
-#                "p99.upper" upper   estimate of the relative change in the 99th percentile of wet-day amounts
-#
-#                 (If delta.file is not provided, predefined deltas are derived dependening on <sc>, <p> and <scaling>)
-#
-# sc             scenario                      ["GL", "GH", "WL", "WH"]
-# p              time horizon                  [2030 (=DEFAULT), 2050, 2085]
-# scaling        scaling extreme precipitation ["lower", "centr" (=DEFAULT), "upper"]
-#
-# version        "v1.1" [DEFAULT] official version that belongs to KNMI'14
-#                "v1.2" alternative procedure to dry wet days
-#
-###########################################################################################################
-
 #' Transformation of precipitation
+#' @description Function reads 'reference data' with daily precipitation sums
+#' [mm] and 'change factors' from input files and applies them to function
+#' 'rr_trans_KNMI14' to obtain 'future time series' that match a certain climate
 #' @inheritParams temperatuur_transformatie_KNMI14
 #' @param delta.file     [optional] Name of file that contains deltas (changes factors for the transformation)
 #'                File should contain following compulsory columns identified with compulsory headers
 #'                "wdf"       relative change in wet-day frequency
-#'                            (wet days are defined as days with 0.1 mm or more precipitation)
-#'                "ave"       relative change in mean precipitation
-#'                "p99.lower" lower   estimate of the relative change in the 99th percentile of wet-day amounts
-#'                "p99.centr" central estimate of the relative change in the 99th percentile of wet-day amounts
-#'                "p99.upper" upper   estimate of the relative change in the 99th percentile of wet-day amounts
+#'                            (wet days are defined as days with 0.1 mm or more precipitation) \cr
+#'                "ave"       relative change in mean precipitation \cr
+#'                "p99.lower" lower   estimate of the relative change in the 99th percentile of wet-day amounts \cr
+#'                "p99.centr" central estimate of the relative change in the 99th percentile of wet-day amounts \cr
+#'                "p99.upper" upper   estimate of the relative change in the 99th percentile of wet-day amounts \cr
 #'
 #'                 (If delta.file is not provided, predefined deltas are derived dependening on <sc>, <p> and <scaling>)
 #' @param scaling        scaling extreme precipitation ["lower", "centr" (=DEFAULT), "upper"]
