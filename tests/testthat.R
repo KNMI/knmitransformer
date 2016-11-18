f8f8a0a33f62d7dcecbea5b01b248e5b1155e6a5
cphotiadou <christiana.photiadou@knmi.nl>
2016-11-18 09:49:43 +0000

    evaporation testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
 # Old test file
-library(testthat)
-library(knmitransformer)
-#
-# test_check("knmitransformer", filter = "precipitation")
-test_check("knmitransformer", filter = "radiation")
-test_check("knmitransformer", filter = "temperature")
-test_check("knmitransformer", filter = "evaporation")
+# library(testthat)
+# library(knmitransformer)
+# #
+# # test_check("knmitransformer", filter = "precipitation")
+# test_check("knmitransformer", filter = "radiation")
+# test_check("knmitransformer", filter = "temperature")
+# test_check("knmitransformer", filter = "evaporation")
--- tests/testthat/test-evaporation.R
+++ tests/testthat/test-evaporation.R
@@ -8,11 +8,11 @@ library(data.table)
 context("evmk transformation - Entire station set")
 
 var="evmk"
-ifile="regressionInput/evaporation/KNMI14____ref_evmk___19810101-20101231_v3.2.txt"
-ifile_tg="regressionInput/temperature/KNMI14____ref_tg___19810101-20101231_v3.2.txt"
-ifile_rsds="regressionInput/radiation/KNMI14____ref_rsds___19810101-20101231_v3.2.txt"
+ifile_tg="tests/testthat/regressionInput/temperature/KNMI14____ref_tg___19810101-20101231_v3.2.txt"
+ifile_rsds="tests/testthat/regressionInput/radiation/KNMI14____ref_rsds___19810101-20101231_v3.2.txt"
 ofile      <- "tmp.txt" # output file - used only temporary
-delta.file <- NA
+delta.file.rsds <- NA
+delta.file.tg <- NA
 regio.file <- "stationstabel" # table that links stations to region
 
 test_that("2030 decadal prediction", {
@@ -29,7 +29,7 @@ test_that("2030 decadal prediction", {
                                    p=p,
                                    regio.file = regio.file)
 
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14___2030_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14___2030_evmk.rds")
 })
 
 test_that("Scenario WL", {
@@ -43,7 +43,7 @@ test_that("Scenario WL", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_WL_2050_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_WL_2050_evmk.rds")
 
   # Regression test based only on the smaller subset used also for the other
   # scenarios - on the web there is an extended version
@@ -55,7 +55,7 @@ test_that("Scenario WL", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_WL_2085_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_WL_2085_evmk.rds")
 })
 
 test_that("Scenario WH", {
@@ -69,7 +69,7 @@ test_that("Scenario WH", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_WH_2050_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_WH_2050_evmk.rds")
 
   p = 2085
   tmp <- droogte_berekening_KNMI14(ifile_tg=ifile_tg, ifile_rsds=ifile_rsds,
@@ -79,7 +79,7 @@ test_that("Scenario WH", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_WH_2085_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_WH_2085_evmk.rds")
 })
 
 test_that("Scenario GH", {
@@ -93,7 +93,7 @@ test_that("Scenario GH", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_GH_2050_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_GH_2050_evmk.rds")
 
   p = 2085
   tmp <- droogte_berekening_KNMI14(ifile_tg=ifile_tg, ifile_rsds=ifile_rsds,
@@ -103,7 +103,7 @@ test_that("Scenario GH", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_GH_2085_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_GH_2085_evmk.rds")
 })
 
 test_that("Scenario GL", {
@@ -117,7 +117,7 @@ test_that("Scenario GL", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_GL_2050_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_GL_2050_evmk.rds")
 
   p = 2085
   tmp <- droogte_berekening_KNMI14(ifile_tg=ifile_tg, ifile_rsds=ifile_rsds,
@@ -127,5 +127,5 @@ test_that("Scenario GL", {
                                    sc=sc,
                                    p=p,
                                    regio.file = regio.file)
-  expect_equal_to_reference(tmp, "regressionOutput/evaporation/KNMI14_GL_2085_evmk.rds")
+  expect_equal_to_reference(tmp, "tests/testthat/regressionOutput/evaporation/KNMI14_GL_2085_evmk.rds")
 })
