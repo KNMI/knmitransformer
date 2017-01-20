0e773a936cebd0b8cc51279fd492144be2e249cb
MartinRoth <marsianus@gmail.com>
2017-01-20 13:48:03 +0100

    Updated imports

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,4 @@
-#' @importFrom data.table fread setkey data.table
+#' @importFrom data.table fread setkey data.table as.data.table ':='
 #' @import futile.logger
 #' @importFrom stats aggregate quantile uniroot
 #' @importFrom utils read.table write.table packageVersion
