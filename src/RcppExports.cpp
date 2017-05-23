3ca0a7091dba72469cac2f6fb82e25e5aa4b715e
cphotiadou <christiana.photiadou@knmi.nl>
2017-05-23 12:48:01 +0000

    changes with roxygen

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -19,3 +19,13 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"knmitransformer_DeterminePowerLawExponentCpp", (DL_FUNC) &knmitransformer_DeterminePowerLawExponentCpp, 4},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_knmitransformer(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
